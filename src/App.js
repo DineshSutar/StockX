88c8b51111b046f2de5a8cb05f6526be47e9426d
Dinesh Sutar <dsutar720@gmail.com>
2019-07-19 01:24:04 +0530

    Added StatusBar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-// @flow
 import React, { PureComponent } from 'react';
 import { Provider } from 'react-redux';
-import { View } from 'react-native';
+import { View, StatusBar } from 'react-native';
 
 import AppNavigator from './AppNavigator';
 import store from './store';
+import COLORS from './utils/colors';
 
-type Props = {};
-export default class App extends PureComponent<Props> {
+export default class App extends PureComponent {
   render() {
     return (
       <Provider store={store}>
         <View style={{ flex: 1 }}>
+          <StatusBar backgroundColor={COLORS.BLUE_GREY_900} barStyle="light-content" />
           <AppNavigator />
         </View>
       </Provider>
